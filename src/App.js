7155f1052de3387f1890a408c690182707ea7ce6
Brennan Newton <brennanmnewton@gmail.com>
2020-11-26 15:10:16 -0500

    Component and route setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import './bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
+
+//vComponents
+import Navbar from './Components/Navbar';
+import Landing from './Components/Landing';
+import ProductList from './Components/ProductList';
+import ProductInfo from './Components/ProductInfo';
+import Cart from './Components/Cart';
+import About from './Components/About';
+import Err from './Components/Err';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Landing}></Route>
+        <Route path="/shop" component={ProductList}></Route>
+        <Route path="/info" component={ProductInfo}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route component={Err}></Route>
+      </Switch>
+    </React.Fragment>
   );
 }
 
---
+++ src/Components/Err.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Err extends Component {
+    render() {
+        return (
+            <div>
+                Hello from Err!
+            </div>
+        )
+    }
+}
