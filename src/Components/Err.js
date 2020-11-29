2a43757c17b8b1deb6d418d6ac4ec50a827d754e
Brennan Newton <brennanmnewton@gmail.com>
2020-11-29 00:30:22 -0500

    Err page setup

--- src/Components/Err.js
+++ src/Components/Err.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Err extends Component {
     render() {
         return (
-            <div>
-                Hello from Err!
+            <div className="text-title text-center mt-5">
+                <h1>404</h1>
             </div>
         )
     }
