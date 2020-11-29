45f2fdac30e568c58ae6b05dc7a61b1a7fafd7c5
Brennan Newton <brennanmnewton@gmail.com>
2020-11-28 23:52:42 -0500

    Landing page setup

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
-
+import landingImg from '../img/Necklace_TP_Opacity.png';
 export default class Landing extends Component {
     render() {
         return (
-            <div>
-                Hello from Landing!
+            <div className="container m-auto p-0">
+                <div className="row">
+                    <div className="col-10 mx-auto col-lg-6">
+                        <h1 className="info-title text-center my-3 my-lg-1">Second Sight Design</h1>
+                        <img src={landingImg} className="w-100"/>
+                    </div>
+                </div>
             </div>
         )
     }
